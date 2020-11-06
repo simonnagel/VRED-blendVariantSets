377fd0dbd442d8d19d02e36a3749eee3894a6912
Simon Nagel <61537975+simonnagel@users.noreply.github.com>
2020-11-06 18:16:42 +0100

    Update VRED-blendVariantSets.py

--- VRED-blendVariantSets.py
+++ VRED-blendVariantSets.py
@@ -1,5 +1,17 @@
-# Script created by Simon Nagel - 2019.11.07
+'''
+DISCLAIMER:
+---------------------------------
+In any case, all binaries, configuration code, templates and snippets of this solution are of "work in progress" character.
+This also applies to GitHub "Release" versions.
+Neither Simon Nagel, nor Autodesk represents that these samples are reliable, accurate, complete, or otherwise valid. 
+Accordingly, those configuration samples are provided “as is” with no warranty of any kind and you use the applications at your own risk.
+Scripted by Simon Nagel
 
+How to use:
+Paste in your VRED Script Editor and Execute
+Execute the Variant with the ending *_blend to blend into that Variant Set
+
+'''
 
 def deleteVariantBlendGroups():
     allVarSets = getGroupedVariantSets()
